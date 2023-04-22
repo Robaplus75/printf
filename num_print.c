da459e53d629b69e34fd53afda12d32fccff02f1
Robel Wondimu <98416443+Robaplus75@users.noreply.github.com>
2023-04-22 19:25:40 +0300

    Debugging

--- num_print.c
+++ num_print.c
@@ -9,16 +9,5 @@
 
 void num_print(unsigned int num, int *pCount)
 {
-	if ((int)num < 0)
-	{
-		_putchar('-');
-		*pCount += 1;
-		num *= -1;
-	}
-	/* check if the num not one digit */
-	if (num / 10)
-		_putInt(num / 10, pCount); /* call the fun again if not one digit */
-	/* print the num in ascii by adding 48  */
-	_putchar(num % 10 + '0');
-	*pCount += 1;
+	/* This was supposed to print integers  */
 }
