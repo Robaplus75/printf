28e06688ad280aba80637363a19c1c4c9922f800
Robel Wondimu <98416443+Robaplus75@users.noreply.github.com>
2023-04-22 18:26:09 +0300

    Debugging

--- num_print.c
+++ num_print.c
@@ -3,36 +3,22 @@
 /**
  * num_print - prints a number
  * @num: the number tobe printed
+ * @pCount: increments the counter
  * Return: none
  */
 
-void num_print(int num)
+void num_print(unsigned int num, int *pCount)
 {
-	char buffer[20];
-	int len = 0, i;
-	char *str;
-
-	if (num < 0)
+	if ((int)num < 0)
 	{
+		_putchar('-');
+		*pCount += 1;
 		num *= -1;
-		buffer[0] = '-';
-		len++;
 	}
-	do {
-		buffer[len++] = (num % 10) + '0';
-		num /= 10;
-	} while (num > 0);
-
-	str = malloc(sizeof(char) * (len + 1));
-	if (str == NULL)
-	{
-		return;
-	}
-	for (i = 0; i < len; i++)
-	{
-		str[i] = buffer[(len - i) - 1];
-	}
-	str[len] = '\0';
-	write(STDOUT_FILENO, str, len);
-	free(str);
+	/* check if the num not one digit */
+	if (num / 10)
+		_putInt(num / 10, pCount); /* call the fun again if not one digit */
+	/* print the num in ascii by adding 48  */
+	_putchar(num % 10 + '0');
+	*pCount += 1;
 }
